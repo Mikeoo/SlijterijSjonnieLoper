126b84458fba4c3218f374c8646e31bc7f348d7c
Mike Overbeek <mikeoverbeek@outlook.com>
2020-11-12 12:00:02 +0100

    Added real email service

--- SlijterijSjonnieLoper/Program.cs
+++ SlijterijSjonnieLoper/Program.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using SlijterijSjonnieLoper.Services;
 
 namespace SlijterijSjonnieLoper
 {
@@ -13,8 +16,22 @@ namespace SlijterijSjonnieLoper
     {
         public static void Main(string[] args)
         {
+            //Execute().Wait();
             CreateHostBuilder(args).Build().Run();
+
         }
+        //static async Task Execute()
+        //{
+        //    var apiKey = "ZEGIKNIET";
+        //    var client = new SendGridClient(apiKey);
+        //    var from = new EmailAddress("example@example.nl", "Example User");
+        //    var subject = "Sending with SendGrid is Fun";
+        //    var to = new EmailAddress("example@examplea", "Example User");
+        //    var plainTextContent = "and easy to do anywhere, even with C#";
+        //    var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+        //    var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+        //    var response = await client.SendEmailAsync(msg);
+        //}
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
