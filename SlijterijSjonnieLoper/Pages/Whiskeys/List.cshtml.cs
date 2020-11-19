c102a9159c729238a52e7e654f3e70de7ae6f3f0
Mike Overbeek <mikeoverbeek@outlook.com>
2020-11-19 10:55:39 +0100

    update list

--- SlijterijSjonnieLoper/Pages/Whiskeys/List.cshtml.cs
+++ SlijterijSjonnieLoper/Pages/Whiskeys/List.cshtml.cs
@@ -11,7 +11,7 @@ using SlijterijSjonnieLoper.Data;
 
 namespace SlijterijSjonnieLoper.Pages.Whiskeys
 {
-    [Authorize(Policy  = "IsAdmin")]
+    //[Authorize(Policy  = "IsAdmin")]
 
     public class ListModel : PageModel
     {
